2fce4d19c89d26f380328f6def3c87d1c42e4773
firebushtree <firebushtree@163.com>
2021-03-16 10:08:47 +0800

    fix(create-modal): :bug: add extra antd style

--- src/create-modal/style/index.tsx
+++ src/create-modal/style/index.tsx
@@ -1,1 +1,9 @@
+import 'antd/lib/modal/style';
+import 'antd/lib/form/style';
+import 'antd/lib/col/style';
+import 'antd/lib/row/style';
+import 'antd/lib/button/style';
+import 'antd/lib/tooltip/style';
+import 'antd/lib/message/style';
+
 import './index.less';
