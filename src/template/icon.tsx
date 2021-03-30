259339dda8bb770bbf7060acdd415ac0c82352ef
firebushtree <firebushtree@163.com>
2021-03-30 11:04:13 +0800

    feat(temaplate): :art: remote icon-font local dependence

--- src/template/icon.tsx
+++ src/template/icon.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import './icon-font/iconfont';
-
+import React, {useEffect} from 'react';
+import classnames from 'classnames';
+import DomUtil from '../utils/dom-util';
 export interface IconProps {
   name: string;
 }
 
 const Icon: React.FC<IconProps> = (props) => {
-  return (
-    <svg className="env-template-icon" aria-hidden="true">
-      <use xlinkHref={`#${props.name}`}></use>
-    </svg>
-  );
+  useEffect(() => {
+    DomUtil.loadCss(
+      '//at.alicdn.com/t/font_1231247_7mfybkm9fwl.css',
+      'base-iconfont',
+    );
+  }, []);
+
+  return <span className={classnames('iconfont', `${props.name}`)}></span>;
 };
 
 export default Icon;
--- src/utils/dom-util.ts
+++ src/utils/dom-util.ts
@@ -18,4 +18,31 @@ export default class DomUtil {
       document.body.appendChild(scriptEl);
     });
   }
+
+  /**
+   * 使用Promise异步的方式加载css文件
+   * @param {string} url 加载的css的url
+   * @param {string} id link元素的id
+   * @return {void}
+   */
+  static loadCss(url: string, id?: string) {
+    return new Promise((resolve) => {
+      if (id && document.querySelector(`#${id}`)) {
+        return;
+      }
+
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+
+      if (id) {
+        link.id = id;
+      }
+
+      link.href = url;
+      link.onload = () => {
+        resolve(null);
+      };
+      document.head.appendChild(link);
+    });
+  }
 }
