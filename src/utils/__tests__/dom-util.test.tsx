ea2638c4679dc7dc90386e689afddff2fa6e1944
owen.huang <firebushtree@163.com>
2021-04-03 09:13:00 +0800

    test(dom-util): :white_check_mark: add loadCss function test

--- src/utils/__tests__/dom-util.test.tsx
+++ src/utils/__tests__/dom-util.test.tsx
@@ -1,15 +1,17 @@
 import DomUtil from '../dom-util';
 
-const vueCDN = 'https://lib.baomitu.com/vue/2.6.11/vue.esm.browser.min.js';
+const vueCDN = 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.8/vue.min.js';
+const bootstrapCDN
+  = 'https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/5.0.0-beta2/css/bootstrap-grid.css';
 
-const clearScriptEl = () => {
-  const scriptEl = document.querySelectorAll('script');
-  scriptEl.forEach((item) => item.remove());
+const clearElementByTagName = (tagName: string) => {
+  const elements = document.querySelectorAll(tagName);
+  elements.forEach((item) => item.remove());
 };
 
 describe('dom-util', () => {
   test('loadJavascript default', () => {
-    clearScriptEl();
+    clearElementByTagName('script');
     const res = DomUtil.loadJavascript('');
     expect(res).toBe(undefined);
 
@@ -20,7 +22,7 @@ describe('dom-util', () => {
   });
 
   test('loadJavascript repeat', () => {
-    clearScriptEl();
+    clearElementByTagName('script');
     const res1 = DomUtil.loadJavascript(vueCDN, 'vue');
     expect(res1 instanceof Promise).toBe(true);
     const res2 = DomUtil.loadJavascript(vueCDN, 'vue');
@@ -28,5 +30,27 @@ describe('dom-util', () => {
 
     const scriptEl = document.querySelectorAll('script');
     expect(scriptEl.length).toBe(1);
+  });
+
+  test('loadCss default', () => {
+    clearElementByTagName('link');
+    const res = DomUtil.loadCss('');
+    expect(res).toBe(undefined);
+
+    DomUtil.loadCss(bootstrapCDN);
+    DomUtil.loadCss(bootstrapCDN);
+    const linkEl = document.querySelectorAll('link');
+    expect(linkEl.length).toBe(2);
+  });
+
+  test('loadCss repeat', () => {
+    clearElementByTagName('link');
+    const res1 = DomUtil.loadCss(bootstrapCDN, 'bootStrap');
+    expect(res1 instanceof Promise).toBe(true);
+    const res2 = DomUtil.loadCss(bootstrapCDN, 'bootStrap');
+    expect(res2).toBe(undefined);
+
+    const linkEl = document.querySelectorAll('link');
+    expect(linkEl.length).toBe(1);
   });
 });
